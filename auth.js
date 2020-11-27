d61adb43d4276bdcfdc0345da39bb32165c11646
vvvcom248 <vvvcom248@gmail.com>
2020-11-27 17:41:14 +0200

    Implemented auth

---
+++ auth.js
@@ -0,0 +1,8 @@
+function authenticate(login,password){
+    if(login==='login'&& password==='password'){
+        return 'You were logget in';
+    }
+    else{
+return 'Login is incorrect';
+    }
+}
